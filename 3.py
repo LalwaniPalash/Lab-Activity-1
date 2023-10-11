c51ab8bfaa871f7ce9da591a44b1c7d4f866733e
LalwaniPalash <147582785+LalwaniPalash@users.noreply.github.com>
2023-10-11 12:29:55 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,20 @@
+import time
+
+print("Welcome to a Rectangle area calculator program!")
+time.sleep(2)
+
+while True:
+    try:
+        l = int(input("Enter the length: "))
+        b = int(input("Enter the breadth: "))
+        if l > 0 and b > 0:
+            break
+        else:
+            print("Invalid Measurements, please input the measurements again.")
+    except ValueError:
+        print("Invalid Integer!")
+
+unit = input("Enter the unit used for the measurements: ")
+
+ans = l * b
+print("The area of this rectangle is " + str(ans) + " " + unit + "²")
